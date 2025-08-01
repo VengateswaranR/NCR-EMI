6e1008d5d31c5422dd2129dd7f5495d65528acd4
VengateswaranR <97583836+VengateswaranR@users.noreply.github.com>
2025-08-01 17:31:40 +0530

    Update fin.js

--- fin.js
+++ fin.js
@@ -4,19 +4,19 @@ function fincal(){
     month=document.getElementById('month').value
     switch(month){
         case '12':
-            var EMI=101*amount/1000
+            var EMI=parseInt(101*amount/1000)
             break
         case '18':
-            var EMI=72.75*amount/1000
+            var EMI=parseInt(72.75*amount/1000)
             break
         case '24':
-            var EMI=59.5*amount/1000
+            var EMI=parseInt(59.5*amount/1000)
             break
         case '30':
-            var EMI=51.25*amount/1000
+            var EMI=parseInt(51.25*amount/1000)
             break
         case '36':
-            var EMI=46*amount/1000
+            var EMI=parseInt(46*amount/1000)
             break
     }
       document.getElementById('EMI').innerHTML =EMI
